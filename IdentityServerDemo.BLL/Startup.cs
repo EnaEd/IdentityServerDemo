4be8aad706c435dbc5f7902617024a3eaefcf92c
Eugene Titov <aixence@externals.net>
2020-12-08 14:45:15 +0200

    Add project files.

---
+++ IdentityServerDemo.BLL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServerDemo.DAL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServerDemo.BLL
+{
+    public static class Startup
+    {
+        public static void RegisterServiceBLL(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterServiceDAL(configuration);
+        }
+    }
+}
---
+++ IdentityServerDemo.DAL/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServerDemo.DAL
+{
+    public static class Startup
+    {
+        public static void RegisterServiceDAL(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
