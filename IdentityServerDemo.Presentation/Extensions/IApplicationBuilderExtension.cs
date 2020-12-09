c7e1eaed4d97a2864550aba1ef5b6866dd24c010
Eugene Titov <aixence@externals.net>
2020-12-09 15:11:42 +0200

    create IdentityServerDemo database

---
+++ IdentityServerDemo.Presentation/Extensions/IApplicationBuilderExtension.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace IdentityServerDemo.Presentation.Extensions
+{
+    public static class IApplicationBuilderExtension
+    {
+        public static void InitializeDatabase(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                if (!context.Clients.Any())
+                {
+                    Config.Config.GetClients().ToList().ForEach(client => context.Clients.Add(client.ToEntity()));
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    Config.Config.ApiScopes.ToList().ForEach(resource => context.ApiScopes.Add(resource.ToEntity()));
+                    context.SaveChanges();
+                }
+
+                //Add other resources here
+
+                //if (!context.IdentityResources.Any())
+                //{
+                //    foreach (var resource in Config.IdentityResources)
+                //    {
+                //        context.IdentityResources.Add(resource.ToEntity());
+                //    }
+                //    context.SaveChanges();
+                //}
+            }
+        }
+    }
+}
--- IdentityServerDemo.Presentation/Startup.cs
+++ IdentityServerDemo.Presentation/Startup.cs
@@ -1,10 +1,13 @@
 ﻿using IdentityServerDemo.BLL;
+using IdentityServerDemo.Presentation.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 namespace IdentityServerDemo.Presentation
 {
@@ -20,11 +23,19 @@ namespace IdentityServerDemo.Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             services.RegisterServiceBLL(_configuration);
-
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
-            .AddDeveloperSigningCredential()
-            .AddInMemoryApiScopes(Config.Config.ApiScopes)
-            .AddInMemoryClients(Config.Config.GetClients());
+            //.AddTestUsers(TestUsers)
+            .AddConfigurationStore(options =>
+                options.ConfigureDbContext = builder => builder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"),
+                  sql => sql.MigrationsAssembly(migrationsAssembly)))
+
+            .AddOperationalStore(options =>
+            options.ConfigureDbContext = builder => builder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"),
+                    sql => sql.MigrationsAssembly(migrationsAssembly)));
+
+            //.AddInMemoryApiScopes(Config.Config.ApiScopes)
+            //.AddInMemoryClients(Config.Config.GetClients());
 
 
 
@@ -36,6 +47,8 @@ namespace IdentityServerDemo.Presentation
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.InitializeDatabase();
 
             app.UseCors(builder =>
             builder.AllowAnyOrigin()
